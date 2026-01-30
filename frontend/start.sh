c6e699faa9d208449525e0651d7e209cb2e779e9
DavidV-coder <DavidViacheslav@icloud.com>
2026-01-30 15:47:17 +0300

    Fix: Use PORT_PLACEHOLDER in nginx config for proper sed replacement

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace PORT placeholder in nginx config
-sed -i "s/\$PORT/${PORT:-80}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/PORT_PLACEHOLDER/${PORT:-80}/g" /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
